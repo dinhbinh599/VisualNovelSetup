9ba2e2513070c23fca12a8342c7d3407a25cd322
tuan.pham <dinhbinh599@gmail.com>
2025-01-03 09:35:40 +0700

    Add button and logo

--- VisualNovelSetup/Views/MainWindow.axaml.cs
+++ VisualNovelSetup/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 
 namespace VisualNovelSetup.Views;
 
@@ -7,5 +10,17 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private async void SelectFolder_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Create and show the folder selection dialog
+        var result = await this.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "Select a Folder",
+            AllowMultiple = true
+        });
+        
+        Console.WriteLine(result[0].Path.LocalPath);
     }
 }
